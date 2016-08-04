da0380ef1f5d0c89f962066fadd2648b8bf6f140
adamliq <adamliq@hotmail.com>
2016-08-04 14:06:47 +1000

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git --version || echo "GIT not installed. Please install GIT"
+apt-get install -y make
 mkdir Cipherscan
 mkdir SSLTest
 mkdir aha
@@ -9,4 +10,4 @@ git init aha
 cd aha && git pull https://github.com/theZiz/aha.git
 cd ../Cipherscan && git pull https://github.com/jvehent/cipherscan.git
 cd ../SSLTest && git pull https://github.com/drwetter/testssl.sh.git
-cd ../aha && :make && :make install
+cd ../aha && make && make install
