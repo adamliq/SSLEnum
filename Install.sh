435c7fe1808d7f92940d30bf3d3b69977649238e
adamliq <adamliq@hotmail.com>
2016-08-04 13:34:04 +1000

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -3,8 +3,10 @@ git --version || echo "GIT not installed. Please install GIT"
 mkdir SSLEnum_Suite
 mkdir SSLEnum_Suite/Cipherscan
 mkdir SSLEnum_Suite/SSLTest
+mkdir SSLEnum_Suite/aha
 git init SSLEnum_Suite/SSLTest
 git init SSLEnum_Suite/Cipherscan
-git pull https://github.com/theZiz/aha.git
+git init SSLEnum_Suite/aha
+cd SSLEnum_Suite/aha && git pull https://github.com/theZiz/aha.git
 cd SSLEnum_Suite/Cipherscan && git pull https://github.com/jvehent/cipherscan.git
 cd SSLEnum_Suite/SSLTest && git pull https://github.com/drwetter/testssl.sh.git
