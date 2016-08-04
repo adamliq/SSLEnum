19eea5aa5c11c5da13f9b7f15053980dd2708cb5
adamliq <adamliq@hotmail.com>
2016-08-04 14:15:32 +1000

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -4,10 +4,13 @@ apt-get install -y make
 mkdir Cipherscan
 mkdir SSLTest
 mkdir aha
+mkdir rdpseccheck
 git init SSLTest
 git init Cipherscan
 git init aha
+git init rdpseccheck
 cd aha && git pull https://github.com/theZiz/aha.git
 cd ../Cipherscan && git pull https://github.com/jvehent/cipherscan.git
 cd ../SSLTest && git pull https://github.com/drwetter/testssl.sh.git
+cd ../rdpseccheck && git pull https://github.com/portcullislabs/rdp-sec-check.git
 cd ../aha && make && make install
